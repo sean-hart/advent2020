d93039bdeacedaa64e17b90e9f0ee5ee0d673ca7
jerryhebert <jerry.hebert@gmail.com>
2020-12-16 18:38:36 -0800

    day12 solutions

---
+++ jerb/day12/part1.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/python3
+
+
+ORDERS = {
+    'N': lambda f, d, a: ((f[0] + a, f[1]), d),
+    'S': lambda f, d, a: ((f[0] - a, f[1]), d),
+    'E': lambda f, d, a: ((f[0], f[1] + a), d),
+    'W': lambda f, d, a: ((f[0], f[1] - a), d),
+    'W': lambda f, d, a: ((f[0], f[1] - a), d),
+    'F': lambda f, d, a: ((f[0] + d[0] * a, f[1] + d[1] * a), d),
+    'L': lambda f, d, a: (f, rotate(d, -a)),
+    'R': lambda f, d, a: (f, rotate(d, a))
+}
+
+
+def parse_orders(lines):
+    for line in lines:
+        yield line[0], int(line[1:])
+
+
+def rotate(direction, degree):
+    x, y = direction
+    for _ in range(int((degree % 360)/90)):
+        x, y = -y, x
+    return x, y
+
+
+def sail(orders):
+    ship = (0, 0)
+    direction = (0, 1)
+    for order in orders:
+        ship, direction = ORDERS[order[0]](ship, direction, order[1])
+    return abs(ship[0]) + abs(ship[1])
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        print(sail(parse_orders(f)))
